84744630bb1320020f82a313ab8dc4950318f5b1
vinay mocharla <vinaymocharla8@gmail.com>
2020-07-13 16:21:19 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, average;
+            Console.WriteLine("enter two numbers");
+            num1 = Convert.ToInt16(Console.ReadLine());
+            num2 = Convert.ToInt16(Console.ReadLine());
+            average = num1 + num2 / 2;
+            Console.WriteLine($"average of 2 numbers is {average}");
+            Console.ReadLine();
+
+        }
+    }
+}
